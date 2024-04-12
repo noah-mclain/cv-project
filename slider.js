4d14c876b4f330bc0ec3de73af34f4260e8479a4
noah-mclain <nadam.30032415@gmail.com>
2024-04-13 01:28:43 +0200

    make text reveal sequential

--- slider.js
+++ slider.js
@@ -24,9 +24,41 @@
 //   revealNextSection();
 // };
 
-window.onload = function () {
+// window.onload = function () {
+//   var revealTextElements = document.querySelectorAll(".reveal-text");
+//   revealTextElements.forEach(function (element) {
+//     element.classList.add("animate");
+//   });
+// };
+
+
+
+
+
+
+function delay(duration) {
+  return new Promise(resolve => setTimeout(resolve, duration));
+}
+
+window.onload = async function () {
   var revealTextElements = document.querySelectorAll(".reveal-text");
-  revealTextElements.forEach(function (element) {
+  for (let element of revealTextElements) {
+    var height = element.scrollHeight;
+
+    var delayDuration = height / 100;
+    // Apply the animation to the element
     element.classList.add("animate");
-  });
+
+    // Apply the animation to the bar
+    var revealBar = element.querySelector(".reveal-bar");
+    revealBar.style.animation = `fill 5s forwards`;
+
+    // Wait for the animation to complete before moving to the next element
+    await delay(delayDuration * 1000);
+  }
 };
+
+
+
+
+
